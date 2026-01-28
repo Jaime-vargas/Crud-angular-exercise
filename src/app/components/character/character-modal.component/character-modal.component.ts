0a532737ec1e2d0a30bd18f4b394f8cf6bfc2de3
Jaime-vargas <jaime.vargas@live.com.mx>
2026-01-28 17:38:26 -0600

    first commit

---
+++ src/app/components/character/character-modal.component/character-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-modal',
+  imports: [],
+  templateUrl: './character-modal.component.html',
+  styleUrl: './character-modal.component.css',
+})
+export class CharacterModalComponent {
+
+}
---
+++ src/app/models/character.interface.ts
@@ -0,0 +1,7 @@
+
+export interface Character {
+  id: number;
+  name: string;
+  role: string;
+  age: number;
+}
